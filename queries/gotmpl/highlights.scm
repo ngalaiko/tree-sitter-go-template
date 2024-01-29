e7d770eab225dba0eaa2c7983aca5c3f3226f2c2
Kai Moschcau <kai.moschcau@blecon.de>
2024-01-29 17:12:10 +0100

    feat(queries): update to new capture names and improve keywords

--- queries/gotmpl/highlights.scm
+++ queries/gotmpl/highlights.scm
@@ -16,18 +16,19 @@
   method: (selector_expression
             field: (field_identifier) @function (#set! priority 101)))
 
+; Builtin functions
+
+(function_call
+  function: (identifier) @function.builtin
+  (#set! priority 101)
+  (#match? @function.builtin "^(and|call|html|index|slice|js|len|not|or|print|printf|println|urlquery|eq|ne|lt|ge|gt|ge)$"))
+
 ; Operators
 
 ([
   "|"
   ":="
   ] @operator (#set! priority 101))
-
-; Builtin functions
-
-((identifier) @function.builtin
-              (#set! priority 101)
-              (#match? @function.builtin "^(and|call|html|index|slice|js|len|not|or|print|printf|println|urlquery|eq|ne|lt|ge|gt|ge)$"))
 
 ; Delimiters
 
@@ -45,21 +46,46 @@
   "("
   ] @punctuation.bracket (#set! priority 101))
 
-; Keywords
+; Actions
 
-([
-  "else"
-  "if"
-  ] @conditional (#set! priority 101))
+(if_action
+  [
+   "if"
+   "else"
+   "else if"
+   "end"
+   ] @keyword.conditional (#set! priority 101))
 
-([
-  "range"
-  "with"
-  "end"
-  "template"
-  "define"
-  "block"
-  ] @keyword (#set! priority 101))
+(range_action
+  [
+   "range"
+   "else"
+   "end"
+   ] @keyword.repeat (#set! priority 101))
+
+(template_action
+  [
+   "template"
+   ] @function.builtin (#set! priority 101))
+
+(block_action
+  [
+   "block"
+   "end"
+   ] @keyword.directive (#set! priority 101))
+
+(define_action
+  [
+   "define"
+   "end"
+   ] @keyword.directive.define (#set! priority 101))
+
+(with_action
+  [
+   "with"
+   "else"
+   "end"
+   ] @keyword.conditional (#set! priority 101))
 
 ; Literals
 
